761e9cb43221112a31aa84b2a352f4bd4505ab0a
Mark Stabler <mark@markstabler.com>
2020-03-18 18:31:16 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,10 @@
 class Post
-  attr_accessor :name, :author
+  attr_accessor :title, :author
 
   @@all = []
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
   end
 
